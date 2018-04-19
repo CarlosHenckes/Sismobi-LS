6bb3f777892a12e45c8871f3979d3adbd919a0c8
Carlos Henckes <henckes@uol.com.br>
2018-04-19 14:31:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sismobi-LS
---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var load = require('express-load');
+var path = require('path');
+var favicon = require('serve-favicon');
+//var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/home');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+load('models') .then('controllers') .then('routes') .into(app);
+
+
+app.listen(3000, function () {     console.log("Aplicação no ar."); });
